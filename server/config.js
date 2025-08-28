58c28946bf23cf76f61ae3d356fd8521ef309998
Donatien Grolaux <dgrolaux@grayswindle.com>
2025-08-28 13:47:01 +0200

    Migrated to codeberg

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    dbfile:"app.db",
-    secret:"mqldsjkf mqlsdkfj mlqsdf kqmslfj",
-    port:8081,
-    maxupload:"200mb",
-    uploadDirectory:"www/uploads",
-    uploadPrefix:"uploads"
-}
